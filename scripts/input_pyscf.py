dd0d58c9f629505a4b5d2d7d709fc2105957f365
Nike Dattani <dattani.nike@gmail.com>
2023-03-28 16:17:00 -0700

    Major improvements to input_psycf.py
    Now UCCSD(T) VIE is correct (although total energies are both 2 milli-Hartree lower than Harding's published values).

--- scripts/input_pyscf.py
+++ scripts/input_pyscf.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-# Author: Nike Dattani, nike@hpqc.org
+# Authors: 
+# 01/23 - 03/23: Nike Dattani, nike@hpqc.org
+# 03/23 - 03/23: Sam Zhuang
 
 import pyscf, numpy as np
 from pyscf import gto, scf, ao2mo, fci, ci, cc
@@ -50,37 +52,41 @@ H  0.9329 0.0000 -0.2728
 '''
 
 # Energy calculation for neutral molecule
-neut_mol = pyscf.M(atom = molecule_xyz, basis = 'def2-TZVPP', verbose=9, spin=0, charge=0, output='out_neut_ccsd.txt')
+neut_mol = pyscf.M(atom = molecule_xyz, basis = 'def2-TZVPP', verbose=9, spin=0, charge=0)
 #neut_mol.symmetry = False
-neut_mhf = scf.UHF(neut_mol).run()                          # Hartree-Fock calculation, mean-field object created
-neut_mhf = stable_opt_internal(neut_mhf)                    # Opimize the HF solution using stability analysis
-neut_mcc = cc.UCCSD(neut_mhf).set(conv_tol=1e-5, frozen=3)  # Post-Hartree-Fock
-neut_tup = neut_mcc.kernel()                                #
-neut_et = neut_mcc.ccsd_t()                                 #
-neut_E = neut_mcc.e_tot + neut_et                           #
+#neut_mhf = neut_mol.UHF().set(conv_tol=1e-10,max_cycle=999,direct_scf_tol=1e-14) # Hartree-Fock calculation, mean-field object created
+neut_mhf = scf.UHF(neut_mol).run()                          
+neut_mhf = stable_opt_internal(neut_mhf)                                         # Opimize the HF solution using stability analysis
+neut_mcc = cc.UCCSD(neut_mhf).set(conv_tol=1e-7, frozen=3)                       # Post-Hartree-Fock
+neut_tup = neut_mcc.kernel()                                                     #
+neut_et  = neut_mcc.ccsd_t()                                                     #
+neut_E   = neut_mcc.e_tot + neut_et                                              #
 
 # Calculation for cation molecule
-cat_mol = pyscf.M(atom = molecule_xyz, basis = 'def2-TZVPP', verbose=9, spin=1, charge=1, output='out_cat_ccsd.txt')
+cat_mol = pyscf.M(atom = molecule_xyz, basis = 'def2-TZVPP', verbose=9, spin=1, charge=1)
 #cat_mol.symmetry = False
-cat_mhf = scf.UHF(cat_mol).run()                            # Hartree-Fock calculation, mean-field object created
-cat_mhf = stable_opt_internal(cat_mhf)                      # Opimize the HF solution using stability analysis
-cat_mcc = cc.UCCSD(cat_mhf).set(conv_tol=1e-5, frozen=3)    # Post-Hartree-Fock
-cat_tup = cat_mcc.kernel()                                  #
-cat_et = neut_mcc.ccsd_t()                                  #
-cat_E = cat_mcc.e_tot + cat_et                              #
-                                                                                                       
-E_diff = cat_E - neut_E                                     # Ionization energy
+#cat_mhf = cat_mol.UHF().set(conv_tol=1e-10,max_cycle=999,direct_scf_tol=1e-14) # Hartree-Fock calculation, mean-field object created
+cat_mhf = scf.UHF(cat_mol).run()                          
+cat_mhf = stable_opt_internal(cat_mhf)                                           # Opimize the HF solution using stability analysis
+cat_mcc = cc.UCCSD(cat_mhf).set(conv_tol=1e-7, frozen=3)                         # Post-Hartree-Fock
+cat_tup = cat_mcc.kernel()                                                       #
+cat_et  = cat_mcc.ccsd_t()                                                       #
+cat_E   = cat_mcc.e_tot + cat_et                                                 #
+                                                                                                      
+E_diff  = cat_E - neut_E                                                         # Ionization energy
 
-print("Neutral molecule E(UCCSD) from output file (Hartrees) =", neut_mcc.e_tot)
-print("Neutral molecule total energy (Hartrees) =", neut_E)
+# print("Neutral molecule SCF energy (hartrees) =", neut_)
+print("Neutral molecule UCCSD energy (hartrees) =", neut_mcc.e_tot)
+print("Neutral molecule UCCSD(T) energy (hartrees) =", neut_E)
 
-print("Cation molecule E(UCCSD) from output file (Hartrees) =", cat_mcc.e_tot)
-print("Cation molecule total energy (Hartrees) =", cat_E)
+# print("Cation molecule SCF energy (hartrees) =", cat_)
+print("Cation molecule UCCSD energy from output file (hartrees) =", cat_mcc.e_tot)
+print("Cation molecule UCCSD(T) energy (hartrees) =", cat_E)
 
-print("UCCSD energy difference (Hartrees)= ", cat_mcc.e_tot-neut_mcc.e_tot)
-print("UCCSD energy difference (eV) = ", 27.2114*(cat_mcc.e_tot-neut_mcc.e_tot))
+print("UCCSD energy difference (hartrees)= ", cat_mcc.e_tot-neut_mcc.e_tot)
+print("UCCSD energy difference (eV) = ", 27.21138624598*(cat_mcc.e_tot-neut_mcc.e_tot)) # Use 27.211386245988(53) eV from 2018 CODATA
 
-print("UCCSD(T) energy difference (Hartrees)= ", E_diff)
-print("UCCSD(T) energy difference (eV) = ", 27.2114*E_diff)
+print("UCCSD(T) energy difference (hartrees)= ", E_diff)
+print("UCCSD(T) energy difference (eV) = ", 27.21138624598*E_diff)                      # Use 27.211386245988(53) eV from 2018 CODATA
 
 
